00ebe51305e2d7495945c0912cf01adad7d287c9
cephaske254 <ctoo@innovexsolutions.co.ke>
2022-10-09 20:00:52 +0300

    skip watiting

--- src/worker/registerServiceWorker.ts
+++ src/worker/registerServiceWorker.ts
@@ -1,4 +1,26 @@
-/* eslint-disable no-restricted-globals */
-async function registerServiceWorker() {}
+import { Workbox } from "workbox-window";
+
+async function registerServiceWorker() {
+  if ("navigator" in window && process.env.NODE_ENV === "production") {
+    const wb = new Workbox("/sw.js");
+    wb.messageSkipWaiting();
+    wb.register();
+  } else if (
+    process.env.NODE_ENV !== "production" &&
+    window.navigator &&
+    navigator.serviceWorker
+  ) {
+    const sw = new URL("sw.js", window.location.origin).href;
+
+    const worker = await navigator.serviceWorker.getRegistration(sw);
+    worker?.unregister().then((state) => {
+      console.log(
+        state
+          ? "Sevice worker unmounted successfully"
+          : "Failed to unmount service worker"
+      );
+    });
+  }
+}
 
 export default registerServiceWorker;
