075f590b73c5e8af1a32d2270a1379a44bdd0e8e
cephaske254 <ctoo@innovexsolutions.co.ke>
2022-10-10 11:43:26 +0300

    re write service worker to use workbox build

---
+++ src/worker/build.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const { generateSW } = require("workbox-build");
+const root = path.resolve(__dirname, "../../");
+
+generateSW({
+  swDest: path.join(root, "build/sw.js"),
+  globDirectory: path.join(root, "build"),
+  clientsClaim: true,
+  skipWaiting: true,
+  sourcemap: false,
+  navigationPreload: true,
+  cleanupOutdatedCaches: true,
+  mode: "production",
+  navigateFallback: "/offline.html",
+  runtimeCaching: [
+    {
+      urlPattern: "/",
+      handler: "NetworkFirst",
+      method: "GET",
+      options: {
+        expiration: {
+          maxAgeSeconds: 86400,
+          maxEntries: 1,
+        },
+        cacheName: "start-url",
+      },
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+      handler: "CacheFirst",
+      method: "GET",
+      options: {
+        cacheName: "google-fonts",
+      },
+    },
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg)*/i,
+      handler: "CacheFirst",
+      method: "GET",
+      options: {
+        cacheName: "images",
+      },
+    },
+    {
+      urlPattern: /\.(?:css|js)$/i,
+      handler: "StaleWhileRevalidate",
+      method: "GET",
+      options: {
+        cacheName: "static-files",
+      },
+    },
+  ],
+}).then(({ count, size, warnings }) => {
+  if (warnings.length > 0) {
+    console.warn(
+      "Warnings encountered while injecting the manifest:",
+      warnings.join("\n")
+    );
+  }
+
+  console.log(
+    `Injected a manifest which will precache ${count} files, totaling ${size} bytes.`
+  );
+});
--- src/worker/registerServiceWorker.ts
+++ src/worker/registerServiceWorker.ts
@@ -1,44 +1,13 @@
-/* eslint-disable no-restricted-globals */
-import { Workbox, WorkboxLifecycleEvent } from "workbox-window";
-
-async function registerServiceWorker(
-  onActivated?: (e: WorkboxLifecycleEvent) => void
-) {
-  if ("navigator" in window && process.env.NODE_ENV === "production") {
+import { Workbox } from "workbox-window";
+async function registerServiceWorker() {
+  if ("serviceWorker" in navigator) {
     const wb = new Workbox("/sw.js");
 
-    const registration = await wb.register();
-
-    wb.addEventListener("redundant", (e) => {
-      onActivated?.(e);
-    });
-
-    wb.addEventListener("installed", (w) => {
-      console.log(self);
-      //@ts-ignore
-      self.skipWaiting();
-    });
-
-    if (registration?.waiting) wb.messageSkipWaiting();
-
-    registration?.addEventListener("updatefound", (e) => {
-      console.log("SW update found");
-      wb.messageSkipWaiting();
-    });
-  } else if (
-    process.env.NODE_ENV !== "production" &&
-    window.navigator &&
-    navigator.serviceWorker
-  ) {
-    const sw = new URL("sw.js", window.location.origin).href;
-
-    const worker = await navigator.serviceWorker.getRegistration(sw);
-    worker?.unregister().then((state) => {
-      console.log(
-        state
-          ? "Sevice worker unmounted successfully"
-          : "Failed to unmount service worker"
-      );
+    wb.register().then((reg) => {
+      if (!reg) return reg;
+      reg.onupdatefound = (e) => {
+        console.log("New update found!");
+      };
     });
   }
 }
