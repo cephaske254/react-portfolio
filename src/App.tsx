2c5750417ec0adea3db51351d83d6fb8d9672095
cephaske254 <cephaske254@gmail.com>
2023-08-23 14:06:08 +0300

    remove workbox serrvice workers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Home from "./pages/Home";
 import { ThemeProvider } from "./theme";
-import registerServiceWorker from "./worker/registerServiceWorker";
 
 function App() {
-  registerServiceWorker();
-
   return (
     <ThemeProvider>
       <CssBaseline enableColorScheme />
--- src/worker/registerServiceWorker.ts
+++ src/worker/registerServiceWorker.ts
@@ -1,55 +1,55 @@
-import { Workbox } from "workbox-window";
+// import { Workbox } from "workbox-window";
 
-const register = async (
-  workerPath: string,
-  { bypass, ...options }: { bypass?: boolean; scope: string }
-) => {
-  if (
-    "navigator" in window &&
-    (bypass || process.env.NODE_ENV === "production")
-  ) {
-    const wb = new Workbox(workerPath, options);
+// const register = async (
+//   workerPath: string,
+//   { bypass, ...options }: { bypass?: boolean; scope: string }
+// ) => {
+//   if (
+//     "navigator" in window &&
+//     (bypass || process.env.NODE_ENV === "production")
+//   ) {
+//     const wb = new Workbox(workerPath, options);
 
-    wb.addEventListener("activated", async function () {
-      await caches.keys().then(function (cacheNames) {
-        return Promise.all(
-          cacheNames
-            .filter(function (_cacheName) {
-              // Return true if you want to remove this cache,
-              // but remember that caches are shared across
-              // the whole origin
-              return true;
-            })
-            .map(function (cacheName) {
-              console.log("clearing cache", cacheName);
+//     wb.addEventListener("activated", async function () {
+//       await caches.keys().then(function (cacheNames) {
+//         return Promise.all(
+//           cacheNames
+//             .filter(function (_cacheName) {
+//               // Return true if you want to remove this cache,
+//               // but remember that caches are shared across
+//               // the whole origin
+//               return true;
+//             })
+//             .map(function (cacheName) {
+//               console.log("clearing cache", cacheName);
 
-              return caches.delete(cacheName);
-            })
-        );
-      });
-    });
+//               return caches.delete(cacheName);
+//             })
+//         );
+//       });
+//     });
 
-    wb.register({ immediate: true });
-  } else if (
-    process.env.NODE_ENV !== "production" &&
-    window.navigator &&
-    navigator.serviceWorker
-  ) {
-    const sw = new URL(workerPath, window.location.origin).href;
+//     wb.register({ immediate: true });
+//   } else if (
+//     process.env.NODE_ENV !== "production" &&
+//     window.navigator &&
+//     navigator.serviceWorker
+//   ) {
+//     const sw = new URL(workerPath, window.location.origin).href;
 
-    const worker = await navigator.serviceWorker.getRegistration(sw);
-    worker?.unregister().then((state) => {
-      console.log(
-        state
-          ? "Sevice worker unmounted successfully"
-          : "Failed to unmount service worker"
-      );
-    });
-  }
-};
+//     const worker = await navigator.serviceWorker.getRegistration(sw);
+//     worker?.unregister().then((state) => {
+//       console.log(
+//         state
+//           ? "Sevice worker unmounted successfully"
+//           : "Failed to unmount service worker"
+//       );
+//     });
+//   }
+// };
 
-async function registerServiceWorker() {
-  register("/sw.js", { scope: "/" });
-}
+// async function registerServiceWorker() {
+//   register("/sw.js", { scope: "/" });
+// }
 
-export default registerServiceWorker;
+// export default registerServiceWorker;
